0108cde10fc09da3ed803063b3608a800792dc51
gen <gen1321@mail.ru>
2017-11-12 08:15:58 +0400

    update database yaml for heroku
    wtf
    
    heroku r u allright?
    
    wtf
    
    what the fuck
    
    should work
    
    with adapter
    
    more test
    
    thats it
    
    wrong adapter
    
    ?
    
    should work
    
    fix seed

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -3,6 +3,9 @@ class CreateAdminService
     user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
       user.password = Rails.application.secrets.admin_password
       user.password_confirmation = Rails.application.secrets.admin_password
+      user.first_name = 'admin'
+      user.last_name = 'admin'
+      user.city = 'Moscow'
       user.admin!
     end
   end
